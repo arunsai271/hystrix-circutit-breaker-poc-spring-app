4d629a5611a4a1d69fa32db149d0f87b5ab59fa6
Mustyala Arun Sai <arunsai.m@corp.techouts.local>
2020-06-24 09:22:42 +0530

    hystrix poc project commit

---
+++ hystrixTest/src/test/java/com/techouts/hystrixtest/HystrixTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techouts.hystrixtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
